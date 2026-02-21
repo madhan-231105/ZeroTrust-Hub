1c1a8f05f5998d65bdc5a2cbd790b095dd3e50e6
madhan-231105 <mathangopi23@gmail.com>
2026-02-21 13:16:38 +0530

    frontend structure is created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 // src/App.jsx
 import React from 'react';
-
+// import navbar from './Components/navbar';
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-md text-center">
         <h1 className="text-3xl font-bold text-gray-800 mb-4">
-          Welcome to React + Tailwind!
+          Welcome to Zero
         </h1>
         <p className="text-gray-600">
           This is your default <code>App.jsx</code> using Tailwind CSS.
